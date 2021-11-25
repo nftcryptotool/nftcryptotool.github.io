e43e708d93a181f880215bb5309fc40e8516007d
derekzuk <derekzuk@gmail.com>
2021-11-24 22:36:25 -0800

    Updates

---
+++ precache-manifest.327dc4b468376f52e5e2bbbe3c795ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89e47c23c19e5ff319f",
+    "url": "/static/css/main.3d8ecde5.chunk.css"
+  },
+  {
+    "revision": "d89e47c23c19e5ff319f",
+    "url": "/static/js/main.d89e47c2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a402132b72d674f2d6c",
+    "url": "/static/css/2.bab4637a.chunk.css"
+  },
+  {
+    "revision": "2a402132b72d674f2d6c",
+    "url": "/static/js/2.2a402132.chunk.js"
+  },
+  {
+    "revision": "d2fbed1f06c5c4fd2da88a90ca8b7a43",
+    "url": "/index.html"
+  }
+];
